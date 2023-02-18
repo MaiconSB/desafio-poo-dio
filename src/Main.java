4ebccd6a02968b108e5d33f09e25b7176e05a11b
Maicon Silveira <maiconsb2041@gmail.com>
2023-02-18 08:36:26 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
+        curso.setTitulo("Java");
+        curso.setDescricao("Curso java OPP");
+        curso.setCargaHoraria(15);
+        System.out.println(curso);
+        System.out.println("------------------------------------------------------------");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("JavaScript");
+        curso1.setDescricao("FRONT");
+        curso1.setCargaHoraria(10);
+        System.out.println(curso1);
+        System.out.println("------------------------------------------------------------");
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+        System.out.println(mentoria);
+        System.out.println("------------------------------------------------------------");
+
+
     }
 }
